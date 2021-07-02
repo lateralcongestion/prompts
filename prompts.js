dbba0cdfef1548f80cf7822e657beebe80cabaa8
lateralcongestion <77700462+lateralcongestion@users.noreply.github.com>
2021-07-01 20:42:43 -0400

    Update prompts.js

--- prompts.js
+++ prompts.js
@@ -1,3 +1,31 @@
+var firebaseConfig = {
+apiKey: "AIzaSyAcsMt473vLZY07Wx9E9qVcvefcElLv_TY",
+authDomain: "okay-5917b.firebaseapp.com",
+projectId: "okay-5917b",
+storageBucket: "okay-5917b.appspot.com",
+messagingSenderId: "501106360406",
+appId: "1:501106360406:web:4ca3cb7c321e9126b63bf7"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database()
+
+function save() {
+  var prompt = document.getElementById('prompt').value.replace(/\n/g, "<br>")
+  var userID = document.getElementById('username').value
+
+  database.ref('users/' + userID).set({
+    userID:userID,
+    prompt : prompt,
+    frompage: location.pathname,
+    when:firebase.database.ServerValue.TIMESTAMP
+  })
+  alert('Saved')
+}
+
+console.log(prompt)
+
 var prompts = 
 [
 "you hear grunting as you slowly walk down the stairs",
@@ -10,7 +38,7 @@ var prompts =
 
 function prose(){
     let x = (prompts[Math.floor(Math.random() * prompts.length)]);
-    document.getElementById("prompt").innerHTML = x;
+    document.getElementById("words").innerHTML = x;
 }
 //function is clicked and triggers
 
